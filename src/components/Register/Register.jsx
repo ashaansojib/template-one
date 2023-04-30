03f6214d73ed63e18c5cb0906107983b1f50b1e8
ashaansojib <ashaduzzamansojib67@gmail.com>
2023-04-30 22:05:30 +0600

    add netlify error code

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../providers/AuthProviders';
 
 
